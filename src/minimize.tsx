f3fc7547cddb6899c80f0f6eb2805cac7f69e108
Marko Gartnar <m.gartnar@sportradar.com>
2022-11-10 15:43:23 +0100

    added a lot of bad code for removing redundant terms from mdno

--- src/minimize.tsx
+++ src/minimize.tsx
@@ -36,12 +36,96 @@ function minimize(minterms: number[], n: number) {
 
 	// remove redundant terms
 
+
 	return removeRedundant(ans);
 }
 
-function removeRedundant(mdno: MDNO) {
-	return mdno;
+function removeRedundant(mdno: MDNO, minterms: number[]) {
+	const ans: MDNO = [];
+
+	for (const minterm of minterms) {
+		let count = 0;
+		let T = [-1, -1];
+
+		for (const M of mdno) {
+			const [term, bitmask] = M;
+			if ((minterm & bitmask) === term) {
+				count++;
+				T = M;
+			}
+		}
+
+		if (count === 1) {
+			ans.push(T);
+		}
+	}
+
+	const rest = [];
+
+	for (const M of mdno) {
+		const [term, bitmask] = M;
+		let isNeeded = false;
+		for (const M2 of ans) {
+			if (term === M2[0] && bitmask === M2[1]) {
+					isNeeded = true;
+			}
+		}
+
+		if (!isNeeded) {
+			rest.push(M);
+		}
+	}
+
+	const otherMinterms = [];
+
+	for (const minterm of minterms) {
+		let isCovered = false;
+		for (const [term, bitmask] of ans) {
+			if ((minterm & bitmask) === term) {
+				isCovered = true;
+				break;
+			}
+		}
+		if (!isCovered) {
+			otherMinterms.push(minterm);
+		}
+	}
+	return removeNontrivialRedundant(ans, otherMinterms);
 }
+
+function removeNontrivialRedundant(mdno : MDNO, minterms: number[], i: number) {
+	// try removing a term from mdno
+	// if it still covers all the minterms call function recursively with the other terms
+	// otherwise do nothing
+	if (i >= mdno.length || minterms.length === 0) return mdno;
+
+
+	for (let i = 0; i < mdno.length; i++) {
+
+	}
+
+	
+}
+
+function mark2(mdno : MDNO, minterms: number[]) {
+	let fullBitmask = 0;
+	for (const minterm of minterms) {
+		fullBitmask = fullBitmask | (1 << minterm);
+	}
+
+	const bitmasks = mdno.map(([term, bitmask]) => {
+		let B = 0;
+		for (const mint of getMinterms()) {
+			B |= (1 << mint);
+		}
+		return B;
+	})
+
+	solve(bitmasks, fullBitmask)
+}
+
+function solve() {}
+
 
 function latexFormat(mdno: MDNO, n: number) {
 	const disjunctions = [];
