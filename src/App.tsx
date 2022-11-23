70c30ec7d577ecb00b727a9045c4e6e65e7eaa5a
Marko Gartnar <marko@Markos-MacBook-Air.local>
2022-11-23 22:24:22 +0100

    commented out things that caused errors

--- src/App.tsx
+++ src/App.tsx
@@ -5,198 +5,222 @@ import Latex from "react-latex-next";
 import { latexFormat, minimize } from "./minimize";
 import cn from "classnames";
 
-const FORMATS = ["circuit", "logic"] as const
+const FORMATS = ["circuit", "logic"] as const;
 export type Format = typeof FORMATS[number];
 
 function getMintermsFromString(mintermsStr: string) {
-  return mintermsStr
-    .replaceAll(",", " ")
-    .split(/\s+/)
-    .filter((chunk) => /\d+/.test(chunk))
-    .map(Number);
+	return mintermsStr
+		.replaceAll(",", " ")
+		.split(/\s+/)
+		.filter((chunk) => /\d+/.test(chunk))
+		.map(Number);
 }
 
 function isValidMintermsString(mintermsStr: string) {
-  return /^(\s*\d+\s*,?\s*)+$/.test(mintermsStr);
+	return /^(\s*\d+\s*,?\s*)+$/.test(mintermsStr);
 }
 
 function isPositiveInt(str: string) {
-  return /\s*\d+\s*/.test(str);
+	return /\s*\d+\s*/.test(str);
 }
 
 function useMinimizer({
-  mintermsStr,
-  numberOfVars,
-  format,
+	mintermsStr,
+	numberOfVars,
+	format,
 }: {
-  mintermsStr: string;
-  numberOfVars: string;
-  format: Format;
+	mintermsStr: string;
+	numberOfVars: string;
+	format: Format;
 }) {
-  const [minterms, setMinterms] = useState([] as number[]);
-  const [n, setN] = useState(1);
+	const [minterms, setMinterms] = useState([] as number[]);
+	const [n, setN] = useState(1);
 
-  useEffect(() => {
-    const isMintermStrValid = isValidMintermsString(mintermsStr);
+	useEffect(() => {
+		const isMintermStrValid = isValidMintermsString(mintermsStr);
 
-    if (isMintermStrValid) {
-      // handle duplicate minterms, larger than or eq 1 << n minterms
-      setMinterms(getMintermsFromString(mintermsStr));
-    }
-
-  }, [mintermsStr]);
+		if (isMintermStrValid) {
+			// handle duplicate minterms, larger than or eq 1 << n minterms
+			setMinterms(getMintermsFromString(mintermsStr));
+		}
+	}, [mintermsStr, numberOfVars]);
 	// enable eslint hook rules
 	useEffect(() => {
 		if (isPositiveInt(numberOfVars)) {
 			setN(parseInt(numberOfVars));
 		}
-	}, [numberOfVars])
+	}, [numberOfVars]);
 
-  const mdno = minimize(minterms, n); //.sort(([term], [term2]) => term - term2);
-  const latex = "$" + String.raw({ raw: latexFormat(mdno, n, format) }) + " $";
+	const mdno = minimize(minterms, n); //.sort(([term], [term2]) => term - term2);
+	let latex = "$" + String.raw({ raw: latexFormat(mdno, n, format) }) + " $";
+
+	if (mdno.length === 1 && mdno[0][1] === 0) {
+		latex = "$ 1 $";
+	}
+
 	// validation
-	const isMintermStrValid = isValidMintermsString(mintermsStr);
+	const isMintermStrValid =
+		isValidMintermsString(mintermsStr) &&
+		minterms.every((minterm) => minterm < 1 << n);
 	const isNValid = isPositiveInt(numberOfVars);
 
-  return { minterms, mdno, n, latex, isMintermStrValid, isNValid };
+	return { minterms, mdno, n, latex, isMintermStrValid, isNValid };
 }
 
 function App() {
-  const [mintermsStr, setMintermsStr] = useState(
-    "1, 4, 6, 7, 8, 9, 10, 11, 15"
-  );
-  const [numberOfVars, setNumberOfVars] = useState("4");
-  const [format, setFormat] = useState<Format>("circuit");
+	const [mintermsStr, setMintermsStr] = useState(
+		"1, 4, 6, 7, 8, 9, 10, 11, 15"
+	);
+	const [numberOfVars, setNumberOfVars] = useState("4");
+	const [format, setFormat] = useState<Format>("circuit");
 
-  const { minterms, n, mdno, latex, isMintermStrValid, isNValid } = useMinimizer({
-    mintermsStr,
-    format,
-    numberOfVars,
-  });
+	const { minterms, n, mdno, latex, isMintermStrValid, isNValid } =
+		useMinimizer({
+			mintermsStr,
+			format,
+			numberOfVars,
+		});
 
-  console.log({ minterms, n, mdno, latex, isMintermStrValid, isNValid });
+	console.log({ minterms, n, mdno, latex, isMintermStrValid, isNValid });
 
-  return (
-    <div className="mx-20 my-16">
-      <h1 className="py-6 text-2xl">PDNO minimizer</h1>
+	return (
+		<div className="mx-20 my-16">
+			<h1 className="py-6 text-2xl">PDNO minimizer</h1>
 
-      <div className="flex gap-6">
-
-        <div className="grid gap-1">
-          <label className="pl-[3px] text-sm" htmlFor="numOfVars">
-            Number of variables
-          </label>
-          <input
-            id="numOfVars"
+			<div className="flex gap-6">
+				<div className="grid gap-1">
+					<label className="pl-[3px] text-sm" htmlFor="numOfVars">
+						Number of variables
+					</label>
+					<input
+						id="numOfVars"
 						type="number"
-            className={cn("rounded-md px-2 w-56 border", {
-              "border-red-300": !isNValid,
-              "outline-red-300": !isNValid,
-            })}
-            placeholder="number of variables"
-            value={numberOfVars}
-            onChange={(e) => setNumberOfVars(e.target.value)}
-          />
-        </div>
+						className={cn("rounded-md px-2 w-56 border", {
+							"border-red-300": !isNValid,
+							"outline-red-300": !isNValid,
+						})}
+						placeholder="number of variables"
+						value={numberOfVars}
+						onChange={(e) => setNumberOfVars(e.target.value)}
+					/>
+				</div>
 
-        <div className="flex">
-          <div className="grid">
-            <label className="pl-[22px] text-sm" htmlFor="minterms">
-              Minterms
-            </label>
-            <div>
-              <Latex>$\lor($</Latex>
-              <input
-                id="minterms"
-                className={cn("rounded-md px-2 w-56 border", {
-                  "border-red-300": !isMintermStrValid,
-                  "outline-red-300": !isMintermStrValid,
-                })}
-                placeholder="minterms"
-                value={mintermsStr}
-                onChange={(e) => setMintermsStr(e.target.value)}
-              />
-              <Latex>$)$</Latex>
-            </div>
-          </div>
-        </div>
+				<div className="flex">
+					<div className="grid">
+						<label className="pl-[22px] text-sm" htmlFor="minterms">
+							Minterms
+						</label>
+						<div>
+							<Latex>$\lor($</Latex>
+							<input
+								id="minterms"
+								className={cn("rounded-md px-2 w-56 border", {
+									"border-red-300": !isMintermStrValid,
+									"outline-red-300": !isMintermStrValid,
+								})}
+								placeholder="minterms"
+								value={mintermsStr}
+								onChange={(e) => setMintermsStr(e.target.value)}
+							/>
+							<Latex>$)$</Latex>
+						</div>
+					</div>
+				</div>
 
 				<div className="grid gap-1">
-          <label className="pl-[3px] text-sm" htmlFor="format">
-            Format (doesnt work yet)
-          </label>
-          <select
-            id="format"
-            className={cn("rounded-md px-2 w-40 border")}
-            placeholder="number of variables"
-            value={format}
-            onChange={(e) => setFormat(e.target.value as Format)}
-          >
-						{FORMATS.map(format => (
-							<option value={format} key={format}>{format}</option>
+					<label className="pl-[3px] text-sm" htmlFor="format">
+						Format (doesnt work yet)
+					</label>
+					<select
+						id="format"
+						className={cn("rounded-md px-2 w-40 border")}
+						placeholder="number of variables"
+						value={format}
+						onChange={(e) => setFormat(e.target.value as Format)}
+					>
+						{FORMATS.map((format) => (
+							<option value={format} key={format}>
+								{format}
+							</option>
 						))}
 					</select>
-        </div>
-
-      </div>
-      <section className="mt-10 grid gap-3">
-        <h3 className="text-xl">Minimized: </h3>
-        <h3 className="pl-4">
-          <Latex>{latex}</Latex>
-        </h3>
-      </section>
-      <section className="mt-16">
-        <VeitchDiagram numberOfVars={n} />
-      </section>
-    </div>
-  );
+				</div>
+			</div>
+			<section className="mt-10 grid gap-3">
+				<h3 className="text-xl">Minimized: </h3>
+				<h3 className="pl-4">
+					<Latex>{latex}</Latex>
+				</h3>
+			</section>
+			<section className="mt-16">
+				<VeitchDiagram numberOfVars={n} />
+			</section>
+		</div>
+	);
 }
 
 function useVeitch(numOfVars: number) {
-
 	const isEven = numOfVars % 2 === 0;
-	const numOfRows = isEven ? numOfVars: numOfVars - 1;
-	const numOfCols = isEven ? numOfVars : 2 * (numOfVars - 1);
+
+	let numOfRows = 1 << Math.floor(numOfVars / 2);
+	let numOfCols = 1 << Math.floor(numOfVars / 2);
+
+	if (!isEven) {
+		numOfCols *= 2;
+	}
+
 	return {
 		numOfRows,
 		numOfCols,
-	}
+	};
 }
 
 function VeitchDiagram({
-  numberOfVars,
+	numberOfVars,
 }: {
-  numberOfVars: number;
-  // minterms: number[];
+	numberOfVars: number;
+	// minterms: number[];
 }) {
-	const {numOfCols, numOfRows} = useVeitch(numberOfVars);
+	const { numOfCols, numOfRows } = useVeitch(numberOfVars);
 
-  const grid = Array(numOfRows).fill(
-    Array(numOfCols).fill(Math.random())
-  );
+	const grid = Array(numOfRows).fill(Array(numOfCols).fill(Math.random()));
 
-  return (
-    <div
-      className="grid max-h-[60vh]"
-      style={{
-        aspectRatio: `${numOfCols}/${numOfRows}`,
-        gridTemplateColumns: `repeat(${numOfCols}, 1fr)`,
-        gridTemplateRows: `repeat(${numOfRows}, 1fr)`,
-      }}
-    >
-      {grid.map((row) =>
-        row.map((value, i) => (
-          <div className="border grid place-items-center" key={i}>
-            {"minterm"}
-          </div>
-        ))
-      )}
-    </div>
-  );
+	// create ranges of variables x1 to xn
+	// seperately for odd and even numbers
+	const segments = [];
+	for (let i = 1; i <= numberOfVars; i++) {
+		segments.push([]);
+		for (let j = 0; j < 1; j++) {
+			// segments[i - 1].push([]);
+		}
+	}
+
+	const getMinterm = (i: number, j: number) => {
+		for (let i = 0; i < numberOfVars; i++) {}
+	};
+
+	return (
+		<div
+			className="grid max-h-[60vh]"
+			style={{
+				aspectRatio: `${numOfCols}/${numOfRows}`,
+				gridTemplateColumns: `repeat(${numOfCols}, 1fr)`,
+				gridTemplateRows: `repeat(${numOfRows}, 1fr)`,
+			}}
+		>
+			{grid.map((row, i) =>
+				row.map((_: any, j: number) => (
+					<div className="border grid place-items-center" key={i + j}>
+						{"mt"}
+					</div>
+				))
+			)}
+		</div>
+	);
 }
 
 function LogicGates() {
-  return <div></div>;
+	return <div></div>;
 }
 
 export default App;
